d0af7b5689799393894e9903ca4e6ad013dd0b0b
gustavokiyatake <43156684+gustavokiyatake@users.noreply.github.com>
2020-10-12 22:19:29 -0300

    Add files via upload

---
+++ Adapter-Pattern1/src/TomadaDeDoisPinos.java
@@ -0,0 +1,5 @@
+public class TomadaDeDoisPinos {
+	public void ligarNaTomadaDeDoisPinos() {
+		System.out.println("Ligado na Tomada de Dois Pinos");
+	}
+}
---
+++ Adapter-Pattern1/src/TomadaDeTresPinos.java
@@ -0,0 +1,5 @@
+public class TomadaDeTresPinos {
+	public void ligarNaTomadaDeTresPinos() {
+		System.out.println("Ligado na Tomada de Tres Pinos");
+	}
+}
